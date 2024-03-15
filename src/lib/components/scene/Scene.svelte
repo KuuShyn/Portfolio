aaab50d85d52f41170f3b8dc72a4ba9f9586f212
KuuShyn <magalonakaj@gmail.com>
2024-03-15 14:11:45 +0800

    host to vercel

---
+++ src/lib/components/scene/Scene.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+    import {T, useThrelte} from '@threlte/core';
+    import {Environment, Float, useTexture} from '@threlte/extras';
+    import Macbook from './Macbook.svelte';
+    import {onMount} from 'svelte';
+
+    export let scroll: number;
+
+    let screenSrc = '/portfolio.png';
+    $: {
+        if (scroll >= 0.1 && scroll <= 0.25) {
+            screenSrc = '/facialRec.png';
+        } else if (scroll > 0.25 && scroll <= 0.5) {
+            screenSrc = '/confeshyn.png';
+        } else if (scroll > 0.5 && scroll <= 0.75) {
+            screenSrc = '/papertoss.png';
+        } else if (scroll > 0.75) {
+            screenSrc = '/shyn.png';
+        } else {
+            screenSrc = '/kuush.png';
+        }
+    }
+
+    const {scene} = useThrelte();
+
+    let fov = 40;
+    onMount(() => {
+        if (window.innerWidth > 640) {
+            fov = 35;
+        }
+    });
+</script>
+
+<svelte:window
+    on:resize={() => {
+        if (window.innerWidth > 640) {
+            fov = 35;
+        } else {
+            fov = 40;
+        }
+    }}
+/>
+
+<T.PerspectiveCamera makeDefault position={[0, 0, 5]} {fov} rotation={[0, 0, 0]} />
+
+<T.DirectionalLight castShadow intensity={0.8} position={[10, 8, 4]} />
+<T.DirectionalLight castShadow intensity={0.3} position={[-5, 6, 15]} />
+<T.AmbientLight intensity={0.3} />
+
+<T.Fog
+    on:create={({ref}) => {
+        scene.fog = ref;
+    }}
+    on:cleanup={() => {
+        scene.fog = null;
+    }}
+    color="#1b1b1b"
+    near={1}
+    far={20}
+/>
+<Environment files="/env.hdr" />
+
+<Float floatingRange={[-0.05, 0.05]} speed={5} rotationSpeed={2} rotationIntensity={0.3}>
+    <Macbook {screenSrc} {scroll} />
+</Float>
